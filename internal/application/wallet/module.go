02aa074b5476ba313832a72e4991acf6275e14c6
novriyantoAli <novriyanto.ali@gmail.com>
2025-12-16 23:05:00 +0800

    Fix wallet feature - cleaned up handler and module
    - Recreated wallet handler with clean implementation
    - Simplified wallet module for FX dependency injection
    - Fixed all compilation errors
    - Handler includes: CreateWallet, GetWallet, GetWallets, AddBalance, WithdrawBalance
    - Properly registered routes for wallet operations

--- internal/application/wallet/module.go
+++ internal/application/wallet/module.go
@@ -5,8 +5,6 @@ import (
 	"github.com/novriyantoAli/cn-wallet/internal/application/wallet/repository"
 	"github.com/novriyantoAli/cn-wallet/internal/application/wallet/service"
 	"go.uber.org/fx"
-	"go.uber.org/zap"
-	"gorm.io/gorm"
 )
 
 var Module = fx.Module(
@@ -17,21 +15,3 @@ var Module = fx.Module(
 		handler.NewWalletHandler,
 	),
 )
-
-type WalletHandler struct {
-	handler *handler.WalletHandler
-}
-
-func NewWalletModule(
-	db *gorm.DB,
-	logger *zap.Logger,
-	h *handler.WalletHandler,
-) *WalletHandler {
-	return &WalletHandler{
-		handler: h,
-	}
-}
-
-func (m *WalletHandler) RegisterRoutes(api interface{}) {
-	// This will be called from the server module
-}
